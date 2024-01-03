c815c97014e0068a82adc8e00b406d533e4c29e2
xonery <kilian.houpeurt@gmail.com>
2024-01-03 06:53:33 -0800

    wip me

---
+++ terraform/main.tf
@@ -0,0 +1,42 @@
+terraform {
+
+  // https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/tunnel
+
+  required_version = ">= 0.13.0"
+
+  required_providers {
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.24.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.12.1"
+    }
+
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.21.0"
+    }
+
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_token
+}
